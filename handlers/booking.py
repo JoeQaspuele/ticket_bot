54cf8df85db8bde3cf450ac2ae05042964eb6484
JoeQaspuele <112169301+JoeQaspuele@users.noreply.github.com>
2025-04-13 21:52:46 +0700

    Update booking.py

--- handlers/booking.py
+++ handlers/booking.py
@@ -1,1 +1,17 @@
+from aiogram import types, Dispatcher
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+import aiosqlite
+from settings import CITY_LIMITS
+from datetime import datetime
+
+class TicketState(StatesGroup):
+    transport_type = State()
+    date_time = State()
+    route = State()
+    flight_info = State()
+    luggage = State()
+    amount = State()
+    confirm = State()
 
