e18a72fc420a05945c319c0e33ab0951381778f5
JoeQaspuele <112169301+JoeQaspuele@users.noreply.github.com>
2025-04-13 22:01:43 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,12 @@
+def format_order_data(order):
+    return (
+        f"{order['full_name']}\n"
+        f"Дата и время отправления: {order['departure_datetime']}\n"
+        f"Маршрут: {order['route1']}"
+        + (f" → {order['route2']}" if order.get("route2") else "") + "\n"
+        f"Номер рейса/поезда: {order['flight_or_train']}\n"
+        f"Транспорт: {order['company']}\n"
+        f"Багаж: {'Включен' if order.get('baggage') else 'Без багажа'}\n"
+        f"Сумма: {order['price']} рублей"
+    )
 
